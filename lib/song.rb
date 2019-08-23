6882d1c727a1c371bea08a13aab2175235bef10c
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-23 02:20:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  @@count=0
+  @@genres=[]
+  attr_reader :name,:artist,:genre
+  def initialize (name,artist,genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+  end
+  
+  def self.count
+    @@count+=1
+  end
+
+  def self.genres
+    
+  
+end
