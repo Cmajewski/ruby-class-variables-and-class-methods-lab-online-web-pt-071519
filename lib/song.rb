f1c56527dd1407460d672fdb4af031ad718ab570
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-23 02:35:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,34 @@
 class Song
   @@count=0
   @@genres=[]
+  @@artists=[]
   attr_reader :name,:artist,:genre
   def initialize (name,artist,genre)
     @name=name
     @artist=artist
     @genre=genre
+    @@count+=1
+    if @@genres.include?(genre)
+    else
+      @@genres<<genre
+    end
+    @@artists<<artist
   end
 
   def self.count
-    @@count+=1
+    @@count
   end
 
   def self.genres
-
+    @@genres
   end
+
+  def self.artist_count
+  end
+
+  def self.genre_count
+  end
+
+  
 
 end
